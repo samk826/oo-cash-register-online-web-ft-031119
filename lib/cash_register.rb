c01f05bb801cec953de73960f54115eca15e1c3d
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-22 17:46:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,10 @@ def add_item(item, price, optional_quantity=1)
 end
 
 def apply_discount
+  if discount !=0
   self.total -= self.total* self.discount/100
   "After the discount, the total comes to $#{self.total}."
+else
+  "There is no discount to apply."
 end
 end

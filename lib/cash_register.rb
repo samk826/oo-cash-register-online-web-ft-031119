d32f9d975bea9f73e7a095794cb502eeda652540
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-22 17:28:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,6 +5,9 @@ def initialize(discount=0)
   @total=0
 end
 
-def add_item(item, price)
-  @total += price
+def add_item(item, price, optional_quantity=1)
+  @total += (price * optional_quantity)
+end
+
+def apply_discount
 end
